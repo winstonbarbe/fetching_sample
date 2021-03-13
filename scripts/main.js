07640890801673d0e95832aed47abe2f2ccb21e2
winstonbarbe <winston.barbe@gmail.com>
2021-03-12 22:05:59 -0500

    allows for frontend video upload and viewing

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,9 @@ import onUpload from "./modules/upload.js";
 import onPageLoad from "./modules/pageLoad.js";
 import chooseVideo from "./modules/videoSelect.js";
 
+// Not Logged In Modules
+import upload from "./modulesNotLogged/upload.js";
+
 const signup = document.querySelector("#signup");
 const login = document.querySelector("#login");
 const logout = document.querySelector(".logout");
@@ -13,7 +16,7 @@ const videoUpload = document.querySelector("#upload");
 
 
 
-videoUpload.addEventListener("change", onUpload);
+videoUpload.addEventListener("change", localStorage.getItem("jwt") ? onUpload : upload);
 videosList.addEventListener("click", chooseVideo);
 
 signup.addEventListener("submit", onSignup);
@@ -23,6 +26,8 @@ logout.addEventListener("click", onLogout);
 if (localStorage.getItem("jwt")) {
   onPageLoad(videosList);
 } 
+
+
 
 
 
---
+++ scripts/modulesNotLogged/upload.js
@@ -0,0 +1,29 @@
+function upload(e) {
+  e.preventDefault();
+  const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/crowdpleaser/video/upload';
+  const CLOUDINARY_UPLOAD_PRESET = 'kvcunjt8';
+  
+  console.log(e.target.files[0]);
+  const file = e.target.files[0];
+  var formData = new FormData();
+  formData.append('file', file);
+  formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+ 
+  axios({
+    url: CLOUDINARY_URL,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: formData
+  }).then(res => {
+    console.log(res);
+    const text = res.data.secure_url;
+    document.querySelector(".viewer").src = text;
+    this.reset();
+  }).catch(err => {
+    console.log(err);
+  });
+}
+
+export default upload;
